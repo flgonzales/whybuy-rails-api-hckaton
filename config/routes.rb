595c4072461b4354547427ea1c58b58b19138901
Ward Verhoef <bypandad@gmail.com>
2016-07-15 16:32:54 +0200

    add user controler for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :user, only: []
+  #resources :users, only: [:index, :show]
   resources :items, only: [:index, :create, :update, :edit, :destroy]
   root 'items#index'
 end
