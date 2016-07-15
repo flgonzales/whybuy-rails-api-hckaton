ab42350109bb4ac327218a3afde2da6b857e2099
Ward Verhoef <bypandad@gmail.com>
2016-07-15 14:39:23 +0200

    add access_token to user, needed for authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160715115110) do
+ActiveRecord::Schema.define(version: 20160715122238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20160715115110) do
     t.string   "user_name"
     t.string   "zipcode"
     t.string   "adress"
+    t.string   "access_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
