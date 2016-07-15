729360a20e77aa16f0fcd128c2e88ab00baa6365
Fabiano Gonzalez <fabianolgonzales@gmail.com>
2016-07-15 12:22:40 +0200

    Items_controller.rb

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,42 @@
+class ItemsController < ApplicationController
+  before_action :set_item, only: [:create, :show, :update, :destroy]
+
+ def index
+   @items = Item.all
+   render json: @items
+ end
+
+ def create
+   @item = Item.new(todo_params)
+     if @item.save
+       render json: @item, status: :created, location: @item
+     else
+       render json: @item.errors, status: :unprocessable_entity
+   end
+ end
+
+ def update
+    @item = Item.find(params[:id])
+     if @item.update(item_params)
+       render json: @item
+     else
+       render json: @item.errors
+     end
+  end
+
+ def destroy
+   @item = Item.find(params[:id])
+   @item.destroy
+   head :no_content
+ end
+
+private
+
+ def set_item
+   @item = Item.find(params[:id])
+ end
+
+ def item_params
+   params.require(:item).permit(:name, :availability)
+ end
+end
